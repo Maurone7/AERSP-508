bd9db1882eec0476b96ce21c87e0ef5585f96b8c
Mauro Patimo <mauro.patimo@gmail.com>
2023-11-29 22:01:19 -0500

    Lecture 25

--- Project_2.py
+++ Project_2.py
@@ -12,5 +12,27 @@ m_list = np.linspace(0, 1, 11)
 beta = lambda m: 2*m/(1+m)
 beta_list = [beta(m) for m in m_list]
 
-for betas in beta_list:
-    
+def rk4(fn, y0, t, dt):
+    """
+    RK4 integrator
+
+    Parameters:
+    - fn: the function to be integrated
+    - y0: the initial value of the state vector
+    - t: the initial time
+    - dt: the time step
+
+    Returns:
+    - y1: the state vector at the next time step
+    """
+
+    k1 = fn(t, y0)
+    k2 = fn(t + 0.5 * dt, y0 + 0.5 * dt * k1)
+    k3 = fn(t + 0.5 * dt, y0 + 0.5 * dt * k2)
+    k4 = fn(t + dt, y0 + dt * k3)
+
+    y1 = y0 + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+    return y1
+
+
